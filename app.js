4cd46c4a367251b8156f0a82f5cce15f503d10c8
Andrii Telishko <telishkoav@gmail.com>
2021-09-07 13:34:25 +0300

    hw is done

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express')
-const cors = require('cors')
 const logger = require('morgan')
+const cors = require('cors')
 
-const api = require('./routes/api/contacts')
+const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
 
-app.use(cors())
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+
 app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
 
-app.use('/api/contacts', api.router)
+app.use('/api/contacts', contactsRouter)
 
-app.listen(4000)
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' })
+})
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message })
+})
+
+module.exports = app
--- model/index.js
+++ model/index.js
@@ -1,24 +1,57 @@
 const fs = require('fs/promises')
-const { v4 } = require('uuid')
-const contactsList = require('./contacts.json')
+const path = require('path')
+
+const contactsPath = path.join(__dirname, './contacts.json')
 
 const listContacts = async () => {
-  try {
-    const data = await fs.readFile(contactsList)
-    const contacts = JSON.parse(data)
-    return contacts
-  } catch (error) {
-    console.log(error.message)
-  }
+  const data = await fs.readFile(contactsPath)
+  const contacts = JSON.parse(data)
+  return contacts
 }
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts()
+  const selectContact = contacts.find(contact => contact.id === +contactId)
+  if (!selectContact) {
+    return null
+  }
+  return selectContact
+}
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts()
+  const idx = contacts.findIndex(contact => contact.id === +contactId)
+  if (idx === -1) {
+    return null
+  }
+  const newContacts = contacts.filter(contact => contact.id !== +contactId)
+  await updateContacts(newContacts)
+  return contacts[idx]
+}
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const newContact = body
+  const contacts = await listContacts()
+  const newContacts = [...contacts, newContact]
+  await updateContacts(newContacts)
+  return newContact
+}
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts()
+  const idx = contacts.findIndex(item => item.id === +contactId)
+  if (idx === -1) {
+    return null
+  }
+  contacts[idx] = { ...contacts[idx], ...body }
+  await updateContacts(contacts)
+  return contacts[idx]
+}
+
+const updateContacts = async (contacts) => {
+  const contactsString = JSON.stringify(contacts)
+  await fs.writeFile(contactsPath, contactsString)
+}
 
 module.exports = {
   listContacts,
