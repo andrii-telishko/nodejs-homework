cb231edc86bb22f8d6d06bebce04ca6038fe6b54
Andrii Telishko <telishkoav@gmail.com>
2021-09-09 14:56:38 +0300

    finish hw 03

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,91 +1,20 @@
 const express = require('express')
-const contactsOperations = require('../../model')
-const contactsSchema = require('../../validation/contactsSchema')
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  try {
-    const products = await contactsOperations.listContacts()
-    res.json({
-      products
-    })
-  } catch (error) {
-    next(error)
-  }
-})
+const { joiSchema } = require('../../model/contact')
+const validation = require('../../validation/validation')
+const ctrl = require('../../controllers/contacts')
 
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params
-    const contact = await contactsOperations.getContactById(contactId)
-    if (!contact) {
-      return res.status(404).json({
-        message: 'Not found'
-      })
-    }
-    res.json({
-      contact
-    })
-  } catch (error) {
-    next(error)
-  }
-})
+router.get('/', ctrl.listContacts)
 
-router.post('/', async (req, res, next) => {
-  try {
-    const { error } = contactsSchema.validate(req.body)
-    if (error) {
-      return res.status(400).json({
-        message: error.message
-      })
-    }
-    const newProduct = await contactsOperations.addContact(req.body)
-    res.status(201).json({
-      newProduct
-    })
-  } catch (error) {
-    next(error)
-  }
-})
+router.get('/:contactId', ctrl.getContactById)
 
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params
-    const deletedContact = await contactsOperations.removeContact(contactId)
-    if (!deletedContact) {
-      return res.status(404).json({
-        message: 'Not found'
-      })
-    }
-    res.json({
-      deletedContact
-    })
-  } catch (error) {
-    next(error)
-  }
-})
+router.post('/', validation(joiSchema), ctrl.addContact)
 
-router.put('/:contactId', async (req, res, next) => {
-  try {
-    const { error } = contactsSchema.validate(req.body)
-    if (error) {
-      return res.status(400).json({
-        message: error.message
-      })
-    }
-    const { contactId } = req.params
-    const updateContact = await contactsOperations.updateContact(contactId, req.body)
-    if (!updateContact) {
-      return res.status(404).json({
-        message: 'Not found'
-      })
-    }
-    res.json({
-      updateContact
-    })
-  } catch (error) {
-    next(error)
-  }
-})
+router.delete('/:contactId', ctrl.removeContact)
+
+router.put('/:contactId', validation(joiSchema), ctrl.updateContact)
+
+router.patch('/:contactId', ctrl.updateStatusContact)
 
 module.exports = router
